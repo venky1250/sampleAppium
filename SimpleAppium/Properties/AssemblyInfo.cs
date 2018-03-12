ed57da75107f4663e95b329c74a4527c23aead23
Gangavarapu <vgangavarapu@dovercorp.com>
2018-03-12 11:26:16 +0530

    first commit

---
+++ SimpleAppium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleAppium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleAppium")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3bdd741f-b647-45c1-8eda-477a62cf4f83")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleAppium/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Threading;
+using Excel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+
+namespace SimpleAppium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //creating instance for appium driver
+        AppiumDriver<IWebElement> driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.SetCapability("deviceName", "donatello");
+            cap.SetCapability("platformVersion", "6.0.0");
+            cap.SetCapability("udid", "172.16.203.149:5555");
+            cap.SetCapability("fullReset", "True");
+            cap.SetCapability(MobileCapabilityType.App, "Browser");
+            cap.SetCapability("platformName", "Andriod");
+
+
+            //ExcelLib.PopulateInCollection(@"D:\VSReadData\ReadData.xlsx");
+
+            FileStream stream = File.Open(@"D:\VSReadData\ReadData.xlsx", FileMode.Open, FileAccess.Read);
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            DataSet result = excelReader.AsDataSet();
+            excelReader.IsFirstRowAsColumnNames = true;
+            DataTable dt = result.Tables[0];
+            string Username = dt.Rows[1][0].ToString();
+            string password = dt.Rows[1][1].ToString();
+            string url = dt.Rows[1][2].ToString();
+
+            //Launch the Android driver
+            driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), cap);
+            driver.Navigate().GoToUrl(url);
+            Thread.Sleep(10000);
+            driver.FindElement(By.Name("username")).SendKeys(Username);
+            Thread.Sleep(5000);
+            driver.FindElement(By.Id("btn-login")).Click();
+            Thread.Sleep(10000);
+            driver.FindElement(By.Id("username")).SendKeys(Username);
+            Thread.Sleep(5000);
+            driver.FindElement(By.Id("password")).SendKeys(password);
+            Thread.Sleep(5000);
+            driver.FindElement(By.Id("signIn")).Click();
+
+            
+
+
+        }
+    }
+}
+    
